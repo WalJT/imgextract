f0ae4291e8e9d48ac17da5a3f0fb87b47217e37f
WalJT <noreply@juliantwalsh.ie>
2024-10-12 16:02:28 +0100

    Add a python file with outline of functions

---
+++ imgextract.py
@@ -0,0 +1,23 @@
+
+"""
+Function to list files in a directory and add
+them to lists or dictionaries or something
+"""
+
+"""
+Function to read date taken from image EXIF data, and store
+this so it can be retrieved with the corresponding image.
+"""
+
+"""
+Create a folder / folders named with the format YYYY-MM-DD - Job Code
+(Get user input for job code(s)) (use existing folder if already there)
+"""
+
+"""
+Copy RAW files directly into the existing or newly created folder.
+Copy JPEGs into a specific JPEG subfolder
+"""
+
+if __name__ == '__main__':
+	print("Hello World")
